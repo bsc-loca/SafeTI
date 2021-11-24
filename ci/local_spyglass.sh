e5c347b703319a3e49bdf4d50f98f51c8110a530
root <guillem.cabo@bsc.es>
2021-11-24 17:31:27 +0000

    WIP spyglass tests

--- ci/local_spyglass.sh
+++ ci/local_spyglass.sh
@@ -8,10 +8,9 @@ EX="${FN#*.}"
 FILE_LIST=""
 #remove tmp folder with same name if any
 rm -rf /tmp/$N
-#make destination folder
+#make destination folders
 mkdir /tmp/$N
-rm -rf $PWD/../../library/
-mkdir $PWD/../../library/
+mkdir /tmp/$N/bsc
 
 ## TODO: work on autogenerating the vhdl projects in a similar way than SV
 echo "#!SPYGLASS_PROJECT_FILE" >> /tmp/$N/$N.prj
@@ -28,20 +27,23 @@ echo "read_file -type vhdl $PWD/../hdl/injector_read_if.vhd" >> /tmp/$N/$N.prj
 echo "read_file -type vhdl $PWD/../hdl/injector_write_if.vhd" >> /tmp/$N/$N.prj
 # Set library name and path
 echo "set_option lib bsc {$PWD/bsc}" >> /tmp/$N/$N.prj
-mkdir /tmp/$N/bsc
-echo "set_option libhdlfiles bsc  {/tmp/$N/bsc}" >> /tmp/$N/$N.prj 
 # BSC library files
 cp ../hdl/injector_pkg.vhd  /tmp/$N/bsc
 echo "set_option libhdlfiles bsc {/tmp/$N/bsc/injector_pkg.vhd}" >> /tmp/$N/$N.prj
 echo "##Common Options Section" >> /tmp/$N/$N.prj
+echo "set_option mthresh 5000000" >> /tmp/$N/$N.prj                             
 echo "set_option projectwdir ." >> /tmp/$N/$N.prj
+echo "set_option top $N" >> /tmp/$N/$N.prj                           
+echo "set_option elab_precompile yes" >> /tmp/$N/$N.prj                         
+echo "set_option hdllibdu yes" >> /tmp/$N/$N.prj                                
 echo "set_option designread_enable_synthesis no" >> /tmp/$N/$N.prj
-echo "set_option language_mode VHDL #mixed" >> /tmp/$N/$N.prj
+echo "set_option language_mode mixed" >> /tmp/$N/$N.prj
 echo "set_option designread_disable_flatten no" >> /tmp/$N/$N.prj
 echo "set_option active_methodology $SPYGLASS_HOME/GuideWare/latest/block/rtl_handoff" >> /tmp/$N/$N.prj
 echo "set_option incdir { ./} " >> /tmp/$N/$N.prj
 echo "set_option handlememory" >> /tmp/$N/$N.prj
 echo "##Goal Setup Section" >> /tmp/$N/$N.prj
+echo "current_methodology $SPYGLASS_HOME/GuideWare/latest/block/rtl_handoff" >> /tmp/$N/$N.prj
 
 cd /tmp/$N;
 export SKIP_PLATFORM_CHECK=TRUE
